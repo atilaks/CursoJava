4a5f8b1697698fd9f0396c3c581e4f76227e514e
jaime <jaimefrancotorrecillas@gmail.com>
2022-05-17 10:10:17 +0200

    update

---
+++ src/main/java/InterfacesGraficas/DibujoEnPanel.java
@@ -0,0 +1,77 @@
+package InterfacesGraficas;
+
+/*
+    - Graphics: Clase para hacer dibujos sencillos en láminas
+        + drawRect: Rectángulos
+        + drawLine: Lineas
+        + drawArc: Arcos
+    - Java2D: Es una biblioteca más actual, que permite más complejidad
+        + Rectangle2D: Rectángulos
+        + Ellipse2D: Círculos
+        + Line2D: Líneas
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+
+public class DibujoEnPanel {
+    public static void main(String args[]){
+        MarcoConDibujosGraphics mi_marco_graphics = new MarcoConDibujosGraphics();
+        mi_marco_graphics.setVisible(true);
+        mi_marco_graphics.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        MarcoConDibujosJava2D mi_marco_java2D = new MarcoConDibujosJava2D();
+        mi_marco_java2D.setVisible(true);
+        mi_marco_java2D.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+class MarcoConDibujosGraphics extends JFrame {
+    public MarcoConDibujosGraphics(){
+        setTitle("Prueba de dibujo");
+        setSize(400, 400);
+        LaminaConFigurasGraphics mi_lamina_graphics = new LaminaConFigurasGraphics();
+        add(mi_lamina_graphics);
+    }
+}
+
+class LaminaConFigurasGraphics extends JPanel {
+    public  void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawRect(50, 50, 200, 200);
+        g.drawLine(100, 100, 300, 200);
+        g.drawArc(50, 100, 100, 200, 120, 150);
+    }
+}
+
+class MarcoConDibujosJava2D extends JFrame {
+    public MarcoConDibujosJava2D(){
+        setTitle("Prueba de dibujo");
+        setBounds(400, 0, 400, 400);
+        LaminaConFigurasJava2D mi_lamina_java2D = new LaminaConFigurasJava2D();
+        add(mi_lamina_java2D);
+    }
+}
+
+class LaminaConFigurasJava2D extends JPanel {
+    public  void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        Rectangle2D rectangulo = new Rectangle2D.Double(100, 100, 200.25, 150);
+        g2.draw(rectangulo);
+        Ellipse2D elipse = new Ellipse2D.Double();
+        elipse.setFrame(rectangulo);
+        g2.draw(elipse);
+        g2.draw(new Line2D.Double(100, 100, 300, 250));
+
+        double CentroEnX = rectangulo.getCenterX();
+        double CentroEnY = rectangulo.getCenterY();
+        double radio = 150;
+        Ellipse2D circulo = new Ellipse2D.Double();
+        circulo.setFrameFromCenter(CentroEnX, CentroEnY, CentroEnX+radio, CentroEnY+radio);
+        g2.draw(circulo);
+    }
+}
--- src/main/java/InterfacesGraficas/Laminas.java
+++ src/main/java/InterfacesGraficas/Laminas.java
@@ -1,15 +1,15 @@
 package InterfacesGraficas;
 
 /*
-    - JPanel: Clase encargada de construir panales
-        + La clase con el panel tiene que heredar de esta
-        + paintComponent: Método para dibujar encima del panel
+    - JPanel: Clase encargada de construir láminas
+        + La clase con la lámina tiene que heredar de esta
+        + paintComponent: Método para dibujar encima de la lámina
  */
 
 import javax.swing.*;
 import java.awt.*;
 
-public class Paneles {
+public class Laminas {
     public static void main(String args[]){
         MarcoConTexto mi_marco = new MarcoConTexto();
         mi_marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -22,12 +22,12 @@ class MarcoConTexto extends JFrame {
         setSize(600, 450);
         setLocation(700, 400);
         setTitle("Primer texto");
-        Panel mi_panel = new Panel();
+        MiLamina mi_panel = new MiLamina();
         add(mi_panel);
     }
 }
 
-class Panel extends JPanel {
+class MiLamina extends JPanel {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         g.drawString("Aprendiendo Swing", 100, 100);
